b3f1cf2534d1fcba085783479d5640edebbec8e0
Infinite-Heaven <3237982838@qq.com>
2025-12-22 14:39:02 +0800

    原始版本

---
+++ src/main/java/lut/luttourismsystem/LuTtourismSystemApplication.java
@@ -0,0 +1,13 @@
+package lut.luttourismsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuTtourismSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuTtourismSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lut/luttourismsystem/LuTtourismSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package lut.luttourismsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuTtourismSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
