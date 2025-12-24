0f540de3e7a6e449bec3889b957270f8fa3d2682
Infinite-Heaven <3237982838@qq.com>
2025-12-24 16:46:49 +0800

    新增链接示范

--- src/main/java/lut/luttourismsystem/Controller/TouristAttractionController.java
+++ src/main/java/lut/luttourismsystem/Controller/TouristAttractionController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/tourist")
+@CrossOrigin(origins = "*")
 public class TouristAttractionController {
 
     @Autowired
